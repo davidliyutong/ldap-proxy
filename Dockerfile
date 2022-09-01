f5ad417816def3fb38588a0f8083ea24fb5a3087
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2022-09-01 17:00:52 +0800

    v0.1a feat: ldap authentication prototype

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ EXPOSE 8080
 WORKDIR /opt/app/
 
 RUN pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
-    pip install ldap3==2.9.1 flask==2.0.2
+    pip install ldap3==2.9.1 flask==2.0.2 flask-httpauth==4.7.0
 COPY src/* /opt/app/
 
 ENTRYPOINT ["python"]
--- src/app.py
+++ src/app.py
@@ -3,9 +3,11 @@
 from ldap3 import Server, Connection, ALL, SUBTREE, ALL_ATTRIBUTES, MODIFY_REPLACE, MODIFY_ADD, MODIFY_DELETE
 from ldap3.utils.hashed import hashed
 from ldap3 import (
-    HASHED_SALTED_SHA, MODIFY_REPLACE
+    HASHED_SALTED_SHA, MODIFY_REPLACE, HASHED_SALTED_SHA256
 )
-import json,os
+
+import json
+import os
 import base64
 from flask import (
     Flask,
@@ -18,20 +20,26 @@ from flask import (
     url_for,
     abort,
 )
+from flask_httpauth import HTTPBasicAuth
 
 
 def status_code(code):
     return Response(status=code)
 
+
 # 这里我是通过docker部署服务，这些配置在环境变量传入，你可以根据需要选择直接填写对应值
 ldap_host = os.getenv("LDAP_HOST")
 port = os.getenv("LDAP_PORT")
-base_dn = os.getenv("BASE_DN")
-user = os.getenv("BIND_USER")
-password = os.getenv("BIND_PASSWORD")
-user_dn = os.getenv("USER_DN") # example: uid={0},ou=users,dc=xxxxx,dc=com
+base_dn = os.getenv("LDAP_BASE_DN")
+user = os.getenv("LDAP_BIND_USER")
+password = os.getenv("LDAP_BIND_PASSWORD")
+# example: uid={0},ou=users,dc=xxxxx,dc=com
+user_dn = os.getenv("LDAP_USER_DN")
+search_dn = os.getenv("LDAP_SEARCH_DN")  # example: (uid={0})
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
 
 class LdapUtils(object):
     def __init__(self, ldap_host=None, port=None, base_dn=None, user=None, password=None):
@@ -51,9 +59,10 @@ class LdapUtils(object):
         obj = self.ldapconn
         search_base = self.base_dn
         search_scope = SUBTREE
-        search_filter = "(cn={0})".format(uid)
+        search_filter = search_dn.format(uid)
         try:
-            obj.search(search_base, search_filter, search_scope, attributes=['cn'], paged_size=1)
+            obj.search(search_base, search_filter, search_scope,
+                       attributes=['cn', 'userPassword'], paged_size=1)
             if len(obj.response) == 1:
                 return obj.response[0]['dn']
             else:
@@ -61,26 +70,18 @@ class LdapUtils(object):
         except Exception as e:
             print(e)
 
-    def ldap_update_pass(self, uid=None, oldpass=None, newpass=None):
-        target_cn = self.ldap_search_dn(uid)
-        try:
-            hashed_password = hashed(HASHED_SALTED_SHA, newpass)
-            print("password:" + hashed_password)
-            changes = {
-                'userPassword': [(MODIFY_REPLACE, [hashed_password])]
-            }
-            return self.ldapconn.modify(target_cn, changes=changes)
-        except Exception as e:
-            print(e)
-            return False
-
     def ldap_get_vaild(self, uid=None, passwd=None):
         if not uid or not passwd:
             return False
+
         obj = self.ldapconn
-        # 这里注意修改成自己ldap上定义的user dn 
-        dn = user_dn.format(uid)
+
+        dn = self.ldap_search_dn(uid)
+        if dn is None:
+            return False
+
         try:
+            # print(dn)
             if obj.rebind(dn, passwd):
                 return True
             else:
@@ -89,32 +90,30 @@ class LdapUtils(object):
             print('e:' + str(e))
 
 
-ldap = LdapUtils(ldap_host, int(port), base_dn, user,password)  
+ldap = LdapUtils(ldap_host, int(port), base_dn, user, password)
 
-@app.route('/auth/<user>/<passwd>')
-def auth_user(user="user", passwd="passwd"):
-    if not ldap.ldap_get_vaild(uid=user,passwd=passwd):
-        return status_code(401)
-    return jsonify(authenticated=True, user=user)
-    
-@app.route('/auth/<qop>/<user>/<passwd>')
-def auth_qop_user(qop=None, user="user", passwd="passwd"):
-    temp = base64.b64decode(passwd.split(" ")[1]).decode()
-    if not ldap.ldap_get_vaild(uid=user,passwd=temp.split(":")[1]):
-        return status_code(401)
-    return jsonify(authenticated=True, user=user)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    auth = request.headers.get('Authorization')
-    if auth is not None:
-        uid = base64.b64decode(auth.split(" ")[1]).decode().split(":")[0]
-        passwd = base64.b64decode(auth.split(" ")[1]).decode().split(":")[1]
-        if not ldap.ldap_get_vaild(uid=uid,passwd=passwd):
-            return status_code(401)
-        return render_template('password.html',uid=uid)
+@auth.error_handler
+def auth_error(status):
     return status_code(401)
 
+
+def ldap_verify_password(username, password):
+    return ldap.ldap_get_vaild(uid=username, passwd=password)
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if ldap_verify_password(username, password):
+        return username
+
+
+@app.route('/auth/ldap')
+@auth.login_required
+def index():
+    return jsonify(authenticated=True, user=auth.current_user())
+
+
 if __name__ == '__main__':
-   app.run(host = '0.0.0.0', port = 8080)
+    app.run(host='0.0.0.0', port=8080)
+    pass
